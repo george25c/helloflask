c7e74b87299cb0028264bf8645138e2a886e1a2e
George Kozak <gkozak@adobe.com>
2012-09-27 23:37:23 -0700

    fixed missing route annotation

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def index():
     return 'Hello World!'
 
-app.route('/hello/')
+@app.route('/hello/')
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
