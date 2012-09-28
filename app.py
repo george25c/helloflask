5edec5e5cd08cbe64e6a5b837a65a850ed96789b
George Kozak <george@25c.com>
2012-09-27 23:07:30 -0700

    added templates

--- app.py
+++ app.py
@@ -4,8 +4,13 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def index():
     return 'Hello World!'
+
+app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
